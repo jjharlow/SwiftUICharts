a180b9b1db1429dbc5780deb72e1b620299ccfce
Jeremy <jjharlow@gmail.com>
2023-06-22 10:31:22 -0500

    Update TouchOverlayBox.swift
    Changed display order of items

--- Sources/SwiftUICharts/SharedLineAndBar/Views/TouchOverlayBox.swift
+++ Sources/SwiftUICharts/SharedLineAndBar/Views/TouchOverlayBox.swift
@@ -9,48 +9,40 @@ import SwiftUI
 
 /**
  View that displays information from the touch events.
+ 6/22/2023 - changed order of infobox items
  */
-internal struct TouchOverlayBox<T: CTChartData>: View {
-    
-    @ObservedObject private var chartData: T
-    
-    @Binding private var boxFrame: CGRect
-    
-    internal init(
-        chartData: T,
-        boxFrame: Binding<CGRect>
-    ) {
-        self.chartData = chartData
-        self._boxFrame = boxFrame
-    }
-    
     internal var body: some View {
         Group {
             if chartData.chartStyle.infoBoxContentAlignment == .vertical {
                 VStack(alignment: .leading, spacing: 0) {
                     ForEach(chartData.infoView.touchOverlayInfo, id: \.id) { point in
-                        chartData.infoDescription(info: point)
-                            .font(chartData.chartStyle.infoBoxDescriptionFont)
-                            .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
-                        chartData.infoValueUnit(info: point)
-                            .font(chartData.chartStyle.infoBoxValueFont)
-                            .foregroundColor(chartData.chartStyle.infoBoxValueColour)
-                        chartData.infoLegend(info: point)
-                            .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
+                        VStack(alignment: .leading, spacing: 4) {
+                            HStack {
+                                Text("\(point.validationTitle): \(point.value)") // Assuming point.validationTitle and point.value are strings
+                                    .font(chartData.chartStyle.infoBoxDescriptionFont)
+                                    .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
+                                Spacer()
+                            }
+                            Text(point.description)
+                                .font(chartData.chartStyle.infoBoxDescriptionFont)
+                                .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
+                        }
                     }
                 }
             } else {
                 HStack {
                     ForEach(chartData.infoView.touchOverlayInfo, id: \.id) { point in
-                        chartData.infoLegend(info: point)
-                            .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
-                            .layoutPriority(1)
-                        chartData.infoDescription(info: point)
-                            .font(chartData.chartStyle.infoBoxDescriptionFont)
-                            .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
-                        chartData.infoValueUnit(info: point)
-                            .font(chartData.chartStyle.infoBoxValueFont)
-                            .foregroundColor(chartData.chartStyle.infoBoxValueColour)
+                        VStack(alignment: .leading, spacing: 4) {
+                            HStack {
+                                Text("\(point.validationTitle): \(point.value)") // Assuming point.validationTitle and point.value are strings
+                                    .font(chartData.chartStyle.infoBoxDescriptionFont)
+                                    .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
+                                Spacer()
+                            }
+                            Text(point.description)
+                                .font(chartData.chartStyle.infoBoxDescriptionFont)
+                                .foregroundColor(chartData.chartStyle.infoBoxDescriptionColour)
+                        }
                     }
                 }
             }
